936cf6ec8927d13a1e803b54c9f4b66a8ec9094d
karya-anak-bangsa <tkab.alamsyah@gmail.com>
2026-05-19 16:14:57 +0700

    Project TKAB

--- database/seeders/HeroSeeder.php
+++ database/seeders/HeroSeeder.php
@@ -14,16 +14,16 @@ class HeroSeeder extends Seeder
 
         # ...
         Hero::create([
-            'title'         => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit',
-            'description'   => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil dolorem eum similique voluptates quis iste iure incidunt commodi molestiae quos? Eligendi, adipisci distinctio! Numquam soluta modi exercitationem, odio iure aliquid.',
-            'image'         => 'hero/hero-1.png',
-            'keywords_1'    => null,
-            'keywords_2'    => null,
-            'keywords_3'    => null,
-            'keywords_4'    => null,
+            'title'         => 'Solusi IT Terbaik untuk Keberlanjutan Bisnis Anda',
+            'description'   => 'Solusi software development, mobile apps, dan pelatihan IT profesional untuk membantu bisnis berkembang di era digital.',
+            'image'         => 'hero/hero-2.png',
+            'keywords_1'    => 'Web Development',
+            'keywords_2'    => 'Mobile Development',
+            'keywords_3'    => 'Training IT',
+            'keywords_4'    => 'Bootcamp',
             'keywords_5'    => null,
-            'cta_1'         => 'Lorem ipsum',
-            'cta_2'         => 'Lorem ipsum',
+            'cta_1'         => 'Konsultasi Gratis',
+            'cta_2'         => 'Lihat Portofolio',
             'status_data'   => 'Active',
         ]);
     }
