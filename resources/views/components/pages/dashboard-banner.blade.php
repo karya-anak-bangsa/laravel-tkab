3aea902d55b0ac0edb22a1f8cabd19343614b16d
karya-anak-bangsa <tkab.alamsyah@gmail.com>
2026-05-24 18:12:06 +0700

    Project TKAB

---
+++ resources/views/components/pages/dashboard-banner.blade.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <div class="col-lg-12">
+        <div class="alert alert-primary" role="alert">
+            <span><i class="fa-solid fa-book mr-2"></i>{{ $slot }}</span>
+        </div>
+        {{-- alert --}}
+    </div>
+    {{-- col --}}
+</div>
+{{-- row --}}
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.backend')
 @section('nav-dashboard', 'active')
 @section('content')
+
+    {{-- Benner --}}
+    <x-pages.dashboard-banner>
+        Selamat Datang di PT. Teknologi Karya Anak Bangsa
+    </x-pages.dashboard-banner>
+
 @endsection
