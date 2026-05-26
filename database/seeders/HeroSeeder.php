7e0bd2f68197142defb1ffc8287a6ed1acd4be96
karya-anak-bangsa <tkab.alamsyah@gmail.com>
2026-05-26 16:24:31 +0700

    Project TKAB

--- database/seeders/HeroSeeder.php
+++ database/seeders/HeroSeeder.php
@@ -15,13 +15,13 @@ class HeroSeeder extends Seeder
         # ...
         Hero::create([
             'title'         => 'Solusi IT Terbaik untuk Keberlanjutan Bisnis Anda',
-            'description'   => 'Kami menghadirkan solusi digital modern yang membantu bisnis berkembang lebih cepat, efisien, dan siap menghadapi era teknologi.',
+            'description'   => '<i>Kami menghadirkan solusi digital modern yang membantu bisnis berkembang lebih cepat, efisien, dan siap menghadapi era teknologi.</i>',
             'image'         => 'hero/hero-4.png',
             'keywords_1'    => 'Web Development',
             'keywords_2'    => 'Mobile Development',
             'keywords_3'    => 'Training IT',
             'keywords_4'    => 'Bootcamp',
-            'keywords_5'    => null,
+            'keywords_5'    => '',
             'cta_1'         => 'Konsultasi Gratis',
             'cta_2'         => 'Lihat Portofolio',
             'status_data'   => 'Active',
